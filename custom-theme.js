cbe5f313118367bdc73d3e5fec161ceae89ff05d
Caden Kraft <58564269+CKraft11@users.noreply.github.com>
2022-03-02 17:36:37 -0600

    Create custom-theme.js

---
+++ custom-theme.js
@@ -0,0 +1,7 @@
+ document.documentElement.style.setProperty('--theme-main', '#214a8d');
+ document.documentElement.style.setProperty('--theme-dark', '#214a8d');
+ document.documentElement.style.setProperty('--theme-lighter', '#214a8d');
+ document.documentElement.style.setProperty('--theme-bg', '#262626');
+ document.documentElement.style.setProperty('--sBr', '3px');
+ document.documentElement.style.setProperty('--mBr', '3px');
+ document.documentElement.style.setProperty('--lBr', '3px');
